765db1cc9e7884c9a07b7990cf0eebb727079370
Daniel <danielest.web@gmail.com>
2026-04-14 16:27:16 -0300

    Criando listas pré-definidas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,30 @@
-const Home = () =>{
-    return(
-        <div>
-            <h1>Minhas Tarefas</h1>
-        </div>
-    )
-}
+import React from "react";
+import TarefaLista from "../components/TarefaLista";
+const Home = () => {
+  const [tarefas, SetTarefas] = React.useState([
+    {
+      id: 1,
+      titulo: "Estudar React",
+      descricao: "Aprender useState",
+      data: "2026-04-14",
+      status: "pendente",
+    },
+    {
+      id: 2,
+      titulo: "Treinar lógica",
+      descricao: "Resolver exercícios",
+      data: "2026-04-15",
+      status: "concluida",
+    },
+  ]);
+
+  return (
+    <div>
+      <h1>Minhas Tarefas</h1>
+
+      <TarefaLista  tarefa={tarefas}/>
+    </div>
+  );
+};
+
+export default Home;
